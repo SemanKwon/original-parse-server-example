4fcfb86b3355089cc9c14edffacdfc7b6d095bd9
seman <hi30000@hanmail.net>
2016-05-18 17:17:27 +0900

    parse-dashboard;

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 var ParseDashboard = require('parse-dashboard');
+var parseServerConfig = require('parse-server-azure-config');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
